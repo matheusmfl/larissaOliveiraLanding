4e14a8295a125851becd34f883732a32f223f217
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-01-29 19:35:37 -0300

    Adicionando fontes, configurando hero image do header e estilizando texto da section 1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,21 +6,23 @@ import heroImage from "../../assets/imgSection1.png";
 export function Header() {
   return (
     <header>
-      <div className="firstContainer">
-        <img src={logo} alt="" className="logo" />
+      <div className="headerContainer">
+        <div className="firstContainer">
+          <img src={logo} alt="" className="logo" />
 
-        <h1>
-          Em apenas 4 horas eu vou te entregar os detalhes mais importantes que
-          ninguém mais te fala sobre a técnica mais pedida nas clínicas de
-          Estética.
-        </h1>
+          <h1>
+            Em apenas 4 horas eu vou te entregar os detalhes mais importantes
+            que ninguém mais te fala sobre a técnica mais pedida nas clínicas de
+            Estética.
+          </h1>
 
-        <Button />
-        <span>Pagamento 100% seguro</span>
-      </div>
+          <Button />
+          <span>Pagamento 100% seguro</span>
+        </div>
 
-      <div>
-        <img src={heroImage} alt=""  />
+        <div>
+          <img src={heroImage} alt="" className="heroImg" />
+        </div>
       </div>
     </header>
   );
